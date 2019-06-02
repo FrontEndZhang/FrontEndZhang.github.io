556d53ff942e5a44209df699568b52ca3a10f679
FrontEngZhang <2393790565@qq.com>
2019-06-02 17:39:59 +0800

    Site updated: 2019-06-02 17:39:53

--- js/util.js
+++ js/util.js
@@ -1,49 +1,78 @@
-var Util = {
-  bind: function(element, name, listener) {
-    element.addEventListener(name, listener, false);
-  },
+const Marked = window.marked;
 
-  addClass: function(element, className) {
-    var classes = element.className ? element.className.split(' ') : [];
-    if (classes.indexOf(className) < 0) {
-      classes.push(className);
-    }
+// Marked.setOptions({
+//   highlight: (code, lang) =>
+//     Prism.highlight(code, Prism.languages[lang || "markup"], lang || "markup")
+// });
 
-    element.className = classes.join(' ');
-    return element;
-  },
+const mdToHTML = content => Marked(content)
 
-  removeClass: function(element, className) {
-    var classes = element.className ? element.className.split(' ') : [];
-    var index = classes.indexOf(className);
-    if (index > -1) {
-      classes.splice(index, 1);
-    }
-
-    element.className = classes.join(' ');
-    return element;
-  },
-
-  request: function(type, url, opts, callback) {
-    var xhr = new XMLHttpRequest();
-    if (typeof opts === 'function') {
-      callback = opts;
-      opts = null;
-    }
-
-    xhr.open(type, url);
-    var fd = new FormData();
-    if (type === 'POST' && opts) {
-      for (var key in opts) {
-        fd.append(key, JSON.stringify(opts[key]));
+const getRealPath = (pathname, desc = false) => {
+  if(!pathname) {
+    pathname = window.location.pathname
+  }
+  let names = pathname.split("/")
+  if(desc === false) {
+    for(let i = names.length - 1; i >= 0; --i) {
+      let name = names[i].trim()
+      if(name.length > 0 && name !== "/" && name !== "index.html") {
+        return name
       }
     }
-
-    xhr.onload = function() {
-      callback(JSON.parse(xhr.response));
-    };
-
-    xhr.send(opts ? fd : null);
+  } else {
+    for(let i = 0 ; i < names.length; ++i) {
+      let name = names[i].trim()
+      if(name.length > 0 && name !== "/" && name !== "index.html") {
+        return name
+      }
+    }
   }
+  return "/"
+}
 
-};
+const generateToc = () => {
+  if(document.body.clientWidth < 768) {
+    return;
+  }
+  $("#sidebar-header").append("<span> Table of Contents </span>");
+  let sidebar = $("#sidebar"),
+    app = $("#app"),
+    topBtn = $(".back-to-top");
+  app.addClass("sidebar-active");
+  sidebar.addClass("sidebar-active");
+  if (document.body.clientWidth <= 768) {
+    topBtn.attr("style", "right: calc(2rem + 200px);");
+  } else {
+    topBtn.attr("style", "right: calc(2rem + 250px);");
+  }
+  topBtn.addClass("sidebar-active");
+  
+  $(".markdown-body")
+    .find("h2,h3,h4,h5,h6")
+    .each(function(i, item) {
+      let tag = $(item).get(0).localName;
+      let tagID = $(item)
+        .text()
+        .replace(/\s{2}/g, "");
+      let idName = $(item).attr("id");
+      $("#sidebar-toc").append(`
+      <li class="toc-${tag}">
+        <a data-id=#${idName}>
+          ${tagID}
+        </a>
+      </li>
+    `);
+    });
+  $("#sidebar-toc").on("click", "li", function() {
+    let idName = $(this)
+      .find("a")
+      .data("id");
+    $("html, body").animate(
+      {
+        scrollTop: $(idName).offset().top - $(".header-wrap").height()
+      },
+      { duration: 500, easing: "swing" }
+    );
+    return false;
+  });
+}
